48a2d3e5b1567e98405858b12f062c7790fa39ed
ShivamKumarG <91918756+ShivamKumarG@users.noreply.github.com>
2024-03-11 14:03:11 +0530

    login api implemented

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,54 +1,116 @@
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../Login/Login.css";
-import { Link } from "react-router-dom";
- 
+
 const LoginForm = () => {
   const [errors, setErrors] = useState({});
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- 
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
+
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
-  const handleSignIn = () => {
-    console.log(email,password)
+
+  const refreshAccessToken = async () => {
+    try {
+      const refreshToken = localStorage.getItem("refreshToken");
+      const response = await fetch('http://localhost:8081/api/v1/auth/refresh', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ refreshToken })
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to refresh access token');
+      }
+
+      const data = await response.json();
+      const newAccessToken = data.accessToken;
+      localStorage.setItem('accessToken', newAccessToken);
+      console.log('Access token refreshed successfully');
+    } catch (error) {
+      console.error('Failed to refresh access token:', error);
+    }
+  };
+
+  const isAccessTokenExpired = () => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+      return true; // No token available, consider it expired
+    }
+
+    const decodedToken = JSON.parse(atob(accessToken.split('.')[1]));
+    const expirationTime = decodedToken.exp * 1000; // Convert seconds to milliseconds
+
+    return expirationTime < Date.now();
+  };
+
+  useEffect(() => {
+    const checkAndRefreshToken = async () => {
+      if (isAccessTokenExpired()) {
+        await refreshAccessToken();
+      }
+    };
+
+    const intervalId = setInterval(checkAndRefreshToken, 15 * 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleSignIn = async () => {
     const errors = {};
     if (!email) {
       errors.email = "Email is required.";
-    } else if(!/^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(email)){
-       errors.email="Enter valid email address"
-     }
- 
+    } else if (!/^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(email)) {
+      errors.email = "Enter valid email address";
+    }
+
     if (!password) {
       errors.password = "Password is required.";
     }
- 
+
     setErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      try {
+        const response = await fetch('http://localhost:8081/api/v1/auth/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        });
+
+        if (!response.ok) {
+          throw new Error('Error occurred during sign-in.');
+        }
+
+        const data = await response.json();
+        const { accessToken, refreshToken } = data;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        console.log('Sign-in successful');
+      } catch (error) {
+        console.error('Sign-in error:', error);
+      }
+    }
   };
- 
+
   return (
     <div className="form">
-    <div class="heading">Login</div><br></br>
+      <div className="heading">Login</div>
+      <br></br>
       <div className="flex-column">
         <label>
           Email <span className="required">*</span>
         </label>
       </div>
       <div className="inputForm">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          viewBox="0 0 32 32"
-          height="20"
-        >
-          <g data-name="Layer 3" id="Layer_3">
-            <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"></path>
-          </g>
-        </svg>
         <input
           className="input"
           type="email"
@@ -60,22 +122,13 @@ const LoginForm = () => {
         />
       </div>
       {errors.email && <p className="error">{errors.email}</p>}
- 
+
       <div className="flex-column">
         <label>
           Password <span className="required">*</span>
         </label>
       </div>
       <div className="inputForm">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          viewBox="-64 0 512 512"
-          height="20"
-        >
-          <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path>
-          <path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path>
-        </svg>
         <input
           className="input"
           type="password"
@@ -88,16 +141,16 @@ const LoginForm = () => {
       </div>
       {errors.password && <p className="error">{errors.password}</p>}
       <div>
-        <button class="button-submit" onClick={handleSignIn}>
+        <button className="button-submit" onClick={handleSignIn}>
           Sign In
         </button>
-        <p class="p">
-          Don't have an account? <Link to="/SignUp"  class="span">Sign Up</Link>
+
+        <p className="p">
+          Don't have an account? <span className="span">Sign Up</span>
         </p>
       </div>
     </div>
   );
 };
- 
+
 export default LoginForm;
- 
